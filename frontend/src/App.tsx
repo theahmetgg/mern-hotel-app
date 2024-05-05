e48c2edd726bd9e8a21951b21b3ad0ff9a4133ad
theahmetgg <ahmet1998gulmez.com>
2024-05-05 23:18:14 +0300

    project files creating

---
+++ frontend/src/App.tsx
@@ -0,0 +1,30 @@
+import { Route, BrowserRouter, Routes, Navigate } from "react-router-dom";
+import Layout from "./layouts/Layout";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <p>Home Page</p>
+            </Layout>
+          }
+        />
+        <Route
+          path="/search"
+          element={
+            <Layout>
+              <p>Search Page</p>
+            </Layout>
+          }
+        />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
